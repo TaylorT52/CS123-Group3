4149380a4db6b73bf8ad0000036d9a5fcaf7a371
Taylor Tam <taylor@taylortam.com>
2025-04-07 17:35:52 -0700

    fixed rotations

--- lab_2.py
+++ lab_2.py
@@ -73,17 +73,17 @@ class ForwardKinematics(Node):
 
 
         #translation matrices 
-        
+
         # T_0_1 (base_link to leg_front_r_1)
         T_0_1 = translation(0.07500, -0.0445, 0) @ rotation_x(1.57080) @ rotation_z(theta1)
 
         # T_1_2 (leg_front_r_1 to leg_front_r_2)
         ## TODO: Implement the transformation matrix from leg_front_r_1 to leg_front_r_2
-        T_1_2 = translation(-0.039, 0, 0) @ rotation_z(-np.pi / 2) 
+        T_1_2 = translation(-0.039, 0, 0) @ rotation_z(-np.pi / 2) @ rotation_y(theta2)
 
         # T_2_3 (leg_front_r_2 to leg_front_r_3)
         ## TODO: Implement the transformation matrix from leg_front_r_2 to leg_front_r_3
-        T_2_3 = translation(0.0685, 0, -0.0494) @ rotation_y(-np.pi / 2)
+        T_2_3 = translation(0.0685, 0, -0.0494) @ rotation_y(-np.pi / 2) @ rotation_y(theta3)
 
         # T_3_ee (leg_front_r_3 to end-effector)
         T_3_ee = translation(-0.018, -0.06216, 0.06231)
