9073b0168bbc652f93086cb4df65cb758e0b4b7b
Rohan Sikand <57341225+rosikand@users.noreply.github.com>
2025-04-14 17:25:37 -0700

    vectorize grad and replace with correct lab1 values

--- lab_3.py
+++ lab_3.py
@@ -86,24 +86,25 @@ class InverseKinematics(Node):
             ])
 
         #translation matrices 
+
         # T_0_1 (base_link to leg_front_r_1)
         T_0_1 = translation(0.07500, -0.0445, 0) @ rotation_x(1.57080) @ rotation_z(theta1)
 
         # T_1_2 (leg_front_r_1 to leg_front_r_2)
         ## TODO: Implement the transformation matrix from leg_front_r_1 to leg_front_r_2
-        T_1_2 = translation(-0.039, 0, 0) @ rotation_z(-np.pi / 2) @ rotation_y(theta2)
+        T_1_2 = translation(0, 0, 0.039) @ rotation_y(-np.pi / 2) @ rotation_z(theta2)
 
         # T_2_3 (leg_front_r_2 to leg_front_r_3)
         ## TODO: Implement the transformation matrix from leg_front_r_2 to leg_front_r_3
-        T_2_3 = translation(0.0685, 0, -0.0494) @ rotation_y(-np.pi / 2) @ rotation_y(theta3)
+        T_2_3 = translation(0, -0.0494, 0.0685) @ rotation_y(np.pi / 2) @ rotation_z(theta3)
 
         # T_3_ee (leg_front_r_3 to end-effector)
-        T_3_ee = translation(-0.018, -0.06216, 0.06231)
+        T_3_ee = translation(0.06231, -0.06216, 0.018)
 
         # TODO: Compute the final transformation. T_0_ee is a concatenation of the previous transformation matrices
         T_0_ee = T_0_1 @ T_1_2 @ T_2_3 @ T_3_ee
 
-        # TODO: Extract the end-effector position. The end effector position is a 3x3 matrix (not in homogenous coordinates)
+        # TODO: Extract the end-effector position.
         end_effector_position = T_0_ee[:3, 3] 
 
         return end_effector_position
@@ -127,7 +128,24 @@ class InverseKinematics(Node):
             return cost, l1dist
 
         def gradient(theta, epsilon=1e-3):
-            return (cost_function(theta + epsilon) - cost_function(theta - epsilon)) / (2*epsilon)
+            # return (cost_function(theta + epsilon) - cost_function(theta - epsilon)) / (2*epsilon)
+
+            # compute the gradient using finite diff 
+            n = len(theta)
+            grad = np.zeros(n)
+            for i in range(n):
+                theta_plus = theta.copy()
+                theta_plus[i] += epsilon
+                cost_plus, _ = cost_function(theta_plus)
+
+                theta_minus = theta.copy()
+                theta_minus[i] -= epsilon
+                cost_minus, _ = cost_function(theta_minus)
+
+                grad[i] = (cost_plus - cost_minus) / (2 * epsilon)
+                    
+            return grad
+            
 
         theta = np.array(initial_guess)
         learning_rate = None # TODO: Set the learning rate
